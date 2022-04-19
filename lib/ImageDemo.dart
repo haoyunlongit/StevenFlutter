03ea4ff041a39550216ba05449f1e7c89fe17953
stevenhao <stevenhao@tencent.com>
2022-04-19 15:56:43 +0800

    图片提交

--- lib/ImageDemo.dart
+++ lib/ImageDemo.dart
@@ -1,17 +1,41 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ImageDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //居中展示
-    return Center(
+    return Row(
       //添加网络图片
-      child: Image.network(
-        //图片url
-        'https://flutter.io/images/flutter-mark-square-100.png',
-        //填充模式
-        fit: BoxFit.fitWidth,
-      ),
+      children: [
+        CachedNetworkImage(
+          imageUrl: "https://vfiles.gtimg.cn/wupload/bbg_business_test.t_bbg_big_horn/20220311_6fhlt5qpd2x.png",
+          imageBuilder: (context, imageProvider) {
+            return Container(
+              width: 60,
+              height: 48,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(6),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1,
+                ),
+              ),
+            );
+          },
+        ),
+        Image.network(
+          //图片url
+          'https://flutter.io/images/flutter-mark-square-100.png',
+          //填充模式
+          fit: BoxFit.fitWidth,
+        ),
+      ],
     );
   }
 }
